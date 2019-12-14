55e2a7dedc9cc49079b97db7e435a7a381c81fa9
rmilne <russmilne@gmail.com>
2019-12-14 13:21:41 -0600

    - Moved neopixel pin to 7 to avoid Serial conflict  - fixed fading code  - added debug per light print values  - Now working with Motion going_up tested

--- stair_lights.ino
+++ stair_lights.ino
@@ -3,7 +3,7 @@
   #include <avr/power.h>
 #endif
 
-#define PIN 0
+#define NEOPIXEL_PIN 7
 #define FSR 0
 #define MOTION 2
 #define ON 1
@@ -12,17 +12,17 @@
 #define DOWN 3
 
 // Stairs settings
-#define UPDATE_DELAY 50 // wait between updates of whole loop
-#define FADE_RATE 30
+#define UPDATE_DELAY 10 // (50 previously) wait between updates of whole loop
+#define FADE_RATE 2
 #define NUM_STAIRS 16
 #define BRIGHTNESS 254
 #define DURATION 10000  // 10 seconds
 #define STAIR_DELAY 600 // .6 seconds between stairs
 #define FSR_THRESHOLD 150 // TODO Needs calibration
-#define DEBUG 1
+#define DEBUG 0
 
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_STAIRS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_STAIRS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
@@ -87,7 +87,6 @@ class Step
             if (duration <= 0 || target_brightness <= 0)
             {
                 state = OFF;
-                target_brightness = 0;
             }
         } 
         if (state == OFF)
@@ -95,15 +94,15 @@ class Step
             target_brightness = 0;
             if (brightness > target_brightness) 
             {
-                brightness = target_brightness - fade;
+                brightness = brightness - fade;
                 if (brightness <= 0)
                 {
                     brightness = 0;
-
                 }
                 strip.setPixelColor(index, brightness, brightness, brightness);	
             }
         }
+        prev_time = curr_time;
     }
 };
 
@@ -146,26 +145,36 @@ class Stairs
         unsigned long curr_time = millis();
         long since_last_update = curr_time - last_stair_on_time;
 
-        // check for stairs turning on in progress
+        // check for stairs turning on in progress       
         if (since_last_update >= stair_delay || since_last_update < 0) 
         {
             if (state == UP) 
             {
-                last_stair_idx++;
                 steps[last_stair_idx]->set(ON, brightness, duration);
-                    if (last_stair_idx == (NUM_STAIRS - 1))
-                    {
-                        state = OFF;
-                    }
+                last_stair_on_time = curr_time;
+                if (last_stair_idx == (NUM_STAIRS - 1))
+                {
+                    state = OFF; // reached the end
+                    last_stair_idx = 0;
+                }
+                else
+                {
+                    last_stair_idx++;
+                }
             }
             if (state == DOWN)
             {
-                last_stair_idx--;
                 steps[last_stair_idx]->set(ON, brightness, duration);
-                    if (last_stair_idx == 0)
-                    {
-                        state = OFF;
-                    }
+                last_stair_on_time = curr_time;
+                if (last_stair_idx == 0)
+                {
+                    state = OFF; // reached the end
+                    last_stair_idx = 0;
+                }
+                else
+                {
+                    last_stair_idx--;
+                }
             }
         }
 
@@ -194,11 +203,30 @@ class Stairs
 
     void print_stairs(unsigned long curr_time)
     {
-        if (curr_time - last_print > 1000 && DEBUG) {
+        if (DEBUG != 1) return;
+        if (curr_time - last_print > 300 && DEBUG) {
             for (int i = 0; i < NUM_STAIRS; i++) {
                 Serial.print(steps[i]->state);
+                Serial.print("\t");
             }
-            Serial.println("");
+            Serial.println("<- state");
+            for (int i = 0; i < NUM_STAIRS; i++) {
+                Serial.print(steps[i]->brightness);
+                Serial.print("\t");
+            }
+            Serial.println("<- brightness");
+            for (int i = 0; i < NUM_STAIRS; i++) {
+                Serial.print(steps[i]->duration);
+                Serial.print("\t");
+            }
+            Serial.println("<- duration");
+            /*
+            for (int i = 0; i < NUM_STAIRS; i++) {
+                Serial.print(strip.getPixelColor(i));
+                Serial.print("\t");
+            }
+            Serial.println("<- pixel color");
+            */
             last_print = curr_time;
         }
     }
@@ -213,9 +241,22 @@ void setup() {
     pinMode(MOTION, INPUT);
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'
+
+    // All on to test LEDs working
+    uint32_t magenta = strip.Color(255, 100, 255);
+    strip.fill(magenta, 0, NUM_STAIRS);
+    strip.show();
+    delay(2000);
+    magenta = strip.Color(0, 0, 0);
+    strip.fill(magenta, 0, NUM_STAIRS);
+    strip.show();
+    
     last_fsr = 0;
     last_motion = 0;
-    Serial.begin(9600);
+    if (DEBUG)
+    {
+        Serial.begin(9600);
+    }
     p("in setup");
 }
 
@@ -241,8 +282,8 @@ void loop() {
         }
     }
     stairs.update();
-    delay(UPDATE_DELAY);
     strip.show();
+    delay(UPDATE_DELAY);   
 }
 
 bool fsr_detect() 
